3f71b7b2c7a063c207709855d8868e0b60cc144d
MiguelCalleja322 <93295694+MiguelCalleja322@users.noreply.github.com>
2022-06-14 01:56:37 +0800

    update

--- lib/Pages/User/feature_page.dart
+++ lib/Pages/User/feature_page.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:eventora/Widgets/custom_profile.dart';
 import 'package:eventora/controllers/feature_page_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class FeaturePage extends StatefulWidget {
   const FeaturePage({Key? key}) : super(key: key);
@@ -12,23 +16,18 @@ class FeaturePage extends StatefulWidget {
 class _FeaturePageState extends State<FeaturePage> {
   late Map<String, dynamic>? features = {};
   late bool loading = false;
-  late List? featuredUsers = [];
-  void fetchFeatures() async {
-    setState(() {
-      loading = true;
-    });
+  late List<dynamic>? featuredUsers = [];
+  late List<dynamic>? featuredOrganizers = [];
 
+  void fetchFeatures() async {
     features = await FeaturePageController().getFeatures();
 
-    featuredUsers = features!['user'];
-
-    featuredUsers!.map((e) {
-      print(e['name']);
-    });
-
     setState(() {
-      loading = false;
+      featuredOrganizers = features!['organizer'];
+      featuredUsers = features!['user'];
     });
+
+    print(featuredOrganizers);
   }
 
   @override
@@ -49,55 +48,117 @@ class _FeaturePageState extends State<FeaturePage> {
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 20.0),
           child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text('Features',
+            child: Container(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text('Features',
                         style:
                             TextStyle(color: Colors.grey[800], fontSize: 40.0)),
-                  ],
-                ),
-                SizedBox(
-                  height: 40,
-                  child: Divider(
-                    color: Colors.grey[600],
                   ),
-                ),
-                const Text(
-                  'Most Followed Organizers',
-                  style: TextStyle(
-                      fontSize: 20.0,
-                      letterSpacing: 2.0,
-                      fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 15),
-                // ListView(scrollDirection: Axis.horizontal, children: [
-                //   CustomProfile(
-                //     image: features!['user']['avatar'],
-                //     name: features!['user']['name'],
-                //     followers: features!['user']['followers_count'],
-                //     followings: features!['user']['following_count'],
-                //   )
-                // ]),
-                const Text(
-                  'Most Followed Users',
-                  style: TextStyle(
-                      fontSize: 20.0,
-                      letterSpacing: 2.0,
-                      fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 15),
-                // const CustomProfile(),
-                const Text(
-                  'Most Visited Events',
-                  style: TextStyle(
-                      fontSize: 20.0,
-                      letterSpacing: 2.0,
-                      fontWeight: FontWeight.bold),
-                ),
-              ],
+                  SizedBox(
+                    height: 40,
+                    child: Divider(
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                  const Text(
+                    'Most Followed Organizers',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        letterSpacing: 2.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 15),
+                  featuredOrganizers!.isEmpty
+                      ? SpinKitCircle(
+                          size: 50.0,
+                          color: Colors.grey[700],
+                        )
+                      : SizedBox(
+                          width: double.infinity,
+                          height: 300,
+                          child: CarouselSlider.builder(
+                              itemCount: featuredOrganizers!.length,
+                              itemBuilder: (context, index, realIndex) {
+                                return SizedBox(
+                                    width: (MediaQuery.of(context).size.width),
+                                    child: CustomProfile(
+                                        // image: featuredUsers![index],
+                                        name: featuredOrganizers![index]
+                                            ['name'],
+                                        followers: featuredOrganizers![index]
+                                                ['followers_count']
+                                            .toString(),
+                                        followings: featuredOrganizers![index]
+                                                ['following_count']
+                                            .toString(),
+                                        events: featuredOrganizers![index]
+                                                ['events_count']
+                                            .toString(),
+                                        role: 'organizer'));
+                              },
+                              options: CarouselOptions(
+                                height: 300,
+                                autoPlay: true,
+                                viewportFraction: 1,
+                                autoPlayInterval: const Duration(seconds: 2),
+                              )),
+                        ),
+                  const Text(
+                    'Most Followed Users',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        letterSpacing: 2.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 15),
+                  featuredOrganizers!.isEmpty
+                      ? SpinKitCircle(
+                          size: 50.0,
+                          color: Colors.grey[700],
+                        )
+                      : SizedBox(
+                          width: double.infinity,
+                          height: 300,
+                          child: CarouselSlider.builder(
+                              itemCount: featuredUsers!.length,
+                              itemBuilder: (context, index, realIndex) {
+                                return SizedBox(
+                                    width: (MediaQuery.of(context).size.width),
+                                    child: CustomProfile(
+                                      // image: featuredUsers![index],
+                                      name: featuredUsers![index]['name'],
+                                      followers: featuredUsers![index]
+                                              ['followers_count']
+                                          .toString(),
+                                      followings: featuredUsers![index]
+                                              ['following_count']
+                                          .toString(),
+                                      events: featuredUsers![index]
+                                              ['events_count']
+                                          .toString(),
+                                      role: 'user',
+                                    ));
+                              },
+                              options: CarouselOptions(
+                                height: 300,
+                                autoPlay: true,
+                                viewportFraction: 1,
+                                autoPlayInterval: const Duration(seconds: 5),
+                              )),
+                        ),
+                  const Text(
+                    'Most Visited Events',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        letterSpacing: 2.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/Widgets/custom_profile.dart
+++ lib/Widgets/custom_profile.dart
@@ -4,132 +4,136 @@ class CustomProfile extends StatelessWidget {
   CustomProfile({
     Key? key,
     this.image,
-    this.name,
-    this.followers,
-    this.followings,
+    required this.name,
+    required this.followers,
+    required this.followings,
     this.events,
+    required this.role,
   }) : super(key: key);
   late String? image = '';
   late String? name = '';
   late String? followers = '';
   late String? followings = '';
   late String? events = '';
+  late String? role = '';
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      height: 400.0,
-      child: DecoratedBox(
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(5)),
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: Column(
-                children: [
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: TextButton(
-                      onPressed: () => null,
-                      child: const Text(
-                        'Follow',
-                        style: TextStyle(
-                            color: Color(0xFF114F5A),
-                            letterSpacing: 1.0,
-                            fontSize: 15.0,
-                            fontWeight: FontWeight.w500),
-                      ),
+    return DecoratedBox(
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(5)),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () => null,
+                    child: const Text(
+                      'Follow',
+                      style: TextStyle(
+                          color: Color(0xFF114F5A),
+                          letterSpacing: 1.0,
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.w500),
                     ),
                   ),
-                  const SizedBox(height: 15),
-                  // Align(
-                  //   alignment: Alignment.center,
-                  //   child: CircleAvatar(
-                  //     backgroundImage: NetworkImage(image!),
-                  //     radius: 90.0,
-                  //   ),
-                  // ),
-                  const SizedBox(height: 15),
-                  Text(
-                    name!,
-                    style: const TextStyle(
-                        color: Color(0xFF114F5A),
-                        letterSpacing: 2.0,
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.w500),
-                  ),
-                  const SizedBox(height: 15),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Column(
-                        children: [
-                          const Text(
-                            'Followers',
-                            style: TextStyle(
-                                color: Color(0xFF114F5A),
-                                letterSpacing: 1.0,
-                                fontSize: 14.0,
-                                fontWeight: FontWeight.w500),
+                ),
+                const SizedBox(height: 15),
+                // Align(
+                //   alignment: Alignment.center,
+                //   child: CircleAvatar(
+                //     backgroundImage: NetworkImage(image!),
+                //     radius: 90.0,
+                //   ),
+                // ),
+                const SizedBox(height: 15),
+                Text(
+                  name!,
+                  style: const TextStyle(
+                      color: Color(0xFF114F5A),
+                      letterSpacing: 2.0,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w500),
+                ),
+                const SizedBox(height: 15),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Column(
+                      children: [
+                        const Text(
+                          'Followers',
+                          style: TextStyle(
+                              color: Color(0xFF114F5A),
+                              letterSpacing: 1.0,
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.w500),
+                        ),
+                        Text(
+                          followers!,
+                          style: const TextStyle(
+                              color: Color(0xFF114F5A),
+                              letterSpacing: 1.0,
+                              fontSize: 26.0,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        const Text(
+                          'Following',
+                          style: TextStyle(
+                              color: Color(0xFF114F5A),
+                              letterSpacing: 1.0,
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.w500),
+                        ),
+                        Text(
+                          followings!,
+                          style: const TextStyle(
+                              color: Color(0xFF114F5A),
+                              letterSpacing: 1.0,
+                              fontSize: 26.0,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ],
+                    ),
+                    role == 'organizer'
+                        ? Column(
+                            children: [
+                              const Text(
+                                'No. Events',
+                                style: TextStyle(
+                                    color: Color(0xFF114F5A),
+                                    letterSpacing: 1.0,
+                                    fontSize: 14.0,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                              Text(
+                                events!,
+                                style: const TextStyle(
+                                    color: Color(0xFF114F5A),
+                                    letterSpacing: 1.0,
+                                    fontSize: 26.0,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            ],
+                          )
+                        : const SizedBox(
+                            width: 0,
+                            height: 0,
                           ),
-                          Text(
-                            followers!,
-                            style: const TextStyle(
-                                color: Color(0xFF114F5A),
-                                letterSpacing: 1.0,
-                                fontSize: 26.0,
-                                fontWeight: FontWeight.w500),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          const Text(
-                            'Following',
-                            style: TextStyle(
-                                color: Color(0xFF114F5A),
-                                letterSpacing: 1.0,
-                                fontSize: 14.0,
-                                fontWeight: FontWeight.w500),
-                          ),
-                          Text(
-                            followings!,
-                            style: const TextStyle(
-                                color: Color(0xFF114F5A),
-                                letterSpacing: 1.0,
-                                fontSize: 26.0,
-                                fontWeight: FontWeight.w500),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          const Text(
-                            'No. Events',
-                            style: TextStyle(
-                                color: Color(0xFF114F5A),
-                                letterSpacing: 1.0,
-                                fontSize: 14.0,
-                                fontWeight: FontWeight.w500),
-                          ),
-                          Text(
-                            events!,
-                            style: const TextStyle(
-                                color: Color(0xFF114F5A),
-                                letterSpacing: 1.0,
-                                fontSize: 26.0,
-                                fontWeight: FontWeight.w500),
-                          ),
-                        ],
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            )
-          ],
-        ),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
