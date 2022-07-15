d17a7b5f598a540c0baac04ec196047fd61e2fb1
MiguelCalleja322 <93295694+MiguelCalleja322@users.noreply.github.com>
2022-07-15 11:26:51 +0800

    main

--- lib/Pages/wall.dart
+++ lib/Pages/wall.dart
@@ -3,6 +3,7 @@ import 'package:eventora/Widgets/custom_event_card_new.dart';
 import 'package:eventora/controllers/event_categories_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:intl/intl.dart';
 
 class WallPage extends StatefulWidget {
@@ -18,20 +19,27 @@ class _WallPageState extends State<WallPage> {
   late Map<String, dynamic>? fetchedCategories = {};
   late List<dynamic>? events = [];
   late String? cloudFrontUri = '';
-
+  late bool? loading = false;
   void fetchCloudFrontUri() async {
     await dotenv.load(fileName: ".env");
     cloudFrontUri = dotenv.env['CLOUDFRONT_URI'];
   }
 
   void getEventsFromCategory() async {
-    fetchedCategories = await EventCategoriesController().show(widget.type);
+    setState(() {
+      loading = true;
+    });
 
+    fetchedCategories = await EventCategoriesController().show(widget.type);
     if (fetchedCategories!.isNotEmpty) {
       setState(() {
         events = fetchedCategories!['event_categories']['events'] ?? [];
       });
     }
+
+    setState(() {
+      loading = false;
+    });
   }
 
   @override
@@ -47,41 +55,50 @@ class _WallPageState extends State<WallPage> {
       appBar: CustomAppBar(
         title: 'Home',
       ),
-      body: events!.isEmpty
+      body: loading == true
           ? Align(
               alignment: Alignment.center,
-              child: Text(
-                'No Events for ${widget.type}',
-                style: const TextStyle(fontSize: 23, color: Colors.black54),
-              ))
-          : SafeArea(
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Column(
-                    children: <Widget>[
-                      ListView.builder(
-                          physics: const NeverScrollableScrollPhysics(),
-                          shrinkWrap: true,
-                          itemCount: events!.length,
-                          itemBuilder: (context, index) {
-                            return CustomEventCard(
-                                slug: events![index]['slug'],
-                                bgColor: int.parse(events![index]['bgcolor']),
-                                imageUrl: cloudFrontUri! +
-                                    events![index]['images'][0],
-                                eventType: events![index]['event_type'],
-                                title: events![index]['title'],
-                                description: events![index]['description'],
-                                dateTime: DateFormat('E, d MMM yyyy HH:mm')
-                                    .format(DateTime.parse(
-                                        events![index]['schedule_start'])));
-                          }),
-                    ],
+              child: SpinKitCircle(
+                size: 50.0,
+                color: Colors.grey[500],
+              ),
+            )
+          : events!.isEmpty
+              ? Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    'No Events for ${widget.type}',
+                    style: const TextStyle(fontSize: 23, color: Colors.black54),
+                  ))
+              : SafeArea(
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        children: <Widget>[
+                          ListView.builder(
+                              physics: const NeverScrollableScrollPhysics(),
+                              shrinkWrap: true,
+                              itemCount: events!.length,
+                              itemBuilder: (context, index) {
+                                return CustomEventCard(
+                                    slug: events![index]['slug'],
+                                    bgColor:
+                                        int.parse(events![index]['bgcolor']),
+                                    imageUrl: cloudFrontUri! +
+                                        events![index]['images'][0],
+                                    eventType: events![index]['event_type'],
+                                    title: events![index]['title'],
+                                    description: events![index]['description'],
+                                    dateTime: DateFormat('E, d MMM yyyy HH:mm')
+                                        .format(DateTime.parse(
+                                            events![index]['schedule_start'])));
+                              }),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
     );
   }
 }
